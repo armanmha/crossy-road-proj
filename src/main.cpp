3902dc1d3c167ecdc1f3f7b1761f0465bdf2302b
BacoJaco <pokemine007craft@gmail.com>
2025-11-18 14:19:54 -0800

    Add leaderboard tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    Menu mainMenu("../data/playerScores.txt");
+    Menu mainMenu("./data/playerScores.txt");
 
     std::cout << "\x1b[3J\x1b[H\x1b[2J" << std::flush;
     mainMenu.run();
--- tests/leaderboardTest.cpp
+++ tests/leaderboardTest.cpp
@@ -10,3 +10,28 @@ TEST(LeaderboardLoadScoresTests, loadInvalidFile) {
     LeaderboardScoreManagement board("banana");
     EXPECT_THROW (board.loadSortedScores(), std::runtime_error);  
 }
+
+TEST(LeaderboardSortingTests, sortScoresDescending) {
+    LeaderboardScoreManagement board("tests/testScores.txt");
+    board.loadSortedScores();
+    const vector<LeaderboardPlayer>& scores = board.getScores();
+    for (size_t i = 1; i < scores.size(); ++i) {
+        EXPECT_LE(scores[i].score, scores[i - 1].score);  
+    }
+}
+
+TEST(LeaderboardDisplayTests, displayTopNScores) {
+    LeaderboardScoreManagement board("tests/testScores.txt");
+    board.loadSortedScores();
+    LeaderboardDisplay display(board);
+    
+    // Capture the output of displayScores (code from CoPilot)
+    testing::internal::CaptureStdout();
+    display.displayScores(3);
+    string output = testing::internal::GetCapturedStdout();
+    
+    // Check if the output contains the top 3 score
+    EXPECT_NE(output.find("1. CCC - 789 (Easy)"), std::string::npos);
+    EXPECT_NE(output.find("2. BBB - 456 (Medium)"), std::string::npos);
+    EXPECT_NE(output.find("3. AAA - 123 (Hard)"), std::string::npos);
+}
